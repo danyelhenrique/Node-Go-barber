5ecd9cc9d4979817db550a13c8a691deee0336f7
danyelhenrique <danyelhenriquefidel@gmail.com>
2019-11-05 17:24:56 -0300

    finish upload avatar user with multer

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,10 +1,21 @@
+const bcrypt = require('bcrypt')
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     avatar: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
     password_hash: DataTypes.STRING,
     provider: DataTypes.BOOLEAN
+  }, {
+    hooks: {
+      beforeSave: async user => {
+        if (user.password) {
+          user.password_hash = await bcrypt.hash(user.password, 8)
+        }
+      }
+    }
   })
   return User
 }
